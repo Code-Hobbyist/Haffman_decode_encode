22af435650c1c7553ba8404b875aec67cdcdc6d3
lijiadao <lijiadao@LAPTOP-4H9QL9DE>
2021-05-15 17:54:39 +0800

    第一次提交代码有误，现更改正确代码

--- Haffman_decode_encode/Haffman.c
+++ Haffman_decode_encode/Haffman.c
@@ -2,72 +2,276 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-#define MAXSIZE 16
-#define LENGTH 27
-typedef struct Shannon_node		//存储概率，码字，码长，信源符号，概率和
-{
-	double sum_probability;
-	double probability;
-	int code_length;
-	char ch;
-	char bit[MAXSIZE];
-}Shannon_node;
-int Shannon_ch;
-double Pro_sum = 0;		//概率累加和
+#define MAXSIZE 27
 int sum = 0;
-char Shannon_code[16] = { '\0' };		//存储香农编码的码字
-char flag = 0;
 double aver_code_length = 0;
-void encode_tofile(Shannon_node bit_code[], char str)		//对文献进行编码
+int index = 0;
+char Haffuman_code[16] = { '\0' };
+int Haffuman_ch = 0;
+/*
+	用链表的方式创建哈夫曼树
+
+==================================
+	定义哈夫曼树的节点信息 ：
+		1.权值
+		2.字符
+		3.位串(存储对应字符的哈夫曼编码)
+		4.定义左右孩子指针
+*/
+typedef struct Tree_Node
 {
-	for (int i = 0; i < LENGTH; i++)
-	{
-		if (bit_code[i].ch == str)
-		{
-			strcpy_s(Shannon_code, sizeof(Shannon_code) / sizeof(Shannon_code[0]), bit_code[i].bit);
-			flag = bit_code[i].code_length;
-		}
-	}
+	double weight;	//权值
+	char ch;	//字符
+	char bit[16];	//位串
+	struct Tree_Node *next, *lChild, *rChild;	//指向下一个节点的指针，左右孩子指针
+}Stack_list;
+/*
+	创建栈，遍历哈夫曼树查找编码时，临时存放对应字符的哈夫曼编码
+*/
+typedef struct Code_Stack {
+	char code[MAXSIZE];		//存放临时对应字符的哈夫曼编码
+	int Stack_top;			//指向栈顶的元素
+}Stack_Code;
+
+//初始化栈
+void init(Stack_Code* Stack)
+{
+	Stack->Stack_top = 0;
 }
-void sort(Shannon_node bit_code[])		//从达到消进行排序
+/*
+	判断栈是否为空 ：
+		Stack->Stack_top为空时，返回1
+		Stack->Stack_top不为空时，返回0
+*/
+int isEmpty(Stack_Code* Stack)
 {
-	for (int i = 0; i < LENGTH; i++)
+	return (Stack->Stack_top ? 0 : 1);
+}
+//入栈
+void Stack_Push(Stack_Code* Stack, char data)
+{
+	if (Stack->Stack_top == MAXSIZE)
 	{
-		for (int j = 0; j < LENGTH - i; j++)
-		{
-			if (bit_code[j].probability < bit_code[j + 1].probability)
-			{
-				Shannon_node temp = bit_code[j + 1];
-				bit_code[j + 1] = bit_code[j];
-				bit_code[j] = temp;
-			}
-		}
+		printf("The stack is full!\n");
 	}
+	Stack->code[Stack->Stack_top] = data;
+	Stack->Stack_top++;
+}
+//	出栈
+void Stack_Pop(Stack_Code* Stack)
+{
+
+	if (isEmpty(Stack))
+	{
+		printf("This stack is empty!\n");
+	}
+	Stack->code[Stack->Stack_top] = '\0';
+	Stack->Stack_top--;
 }
 
+//哈夫曼编码函数
+void Haffuman_encode(Stack_list* root, Stack_Code* Stack)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	/*
+		当检索到的节点左右孩子为NULL是，说明它是叶子节点
+	*/
+	if (root->lChild == NULL && root->rChild == NULL)
+	{
+		int i = 0;
+		/*
+			为了方便拷贝操作，将栈底定义在数组下标为0的位置，
+			这样可以直接将栈中的所有元素拷贝到对应叶子节点的bit数组中
+		*/
+		strcpy_s(root->bit, sizeof(root->bit) / sizeof(root->bit[0]), Stack->code);
+
+	}
+
+	/*
+		哈夫曼编码定义为：左0，右1
+		如果左孩子不为空，将'0'字符入栈，继续递归。
+		如果右孩子不为空，将'1'字符入栈，继续递归。
+		当遍历到对应的叶子节点时，左右孩子为空，当前节点出栈，返回上次的节点，继续递归遍历。
+	*/
+	if (root->lChild != NULL)
+	{
+		Stack_Push(Stack, '0');
+		Haffuman_encode(root->lChild, Stack);
+	}
+	if (root->rChild != NULL)
+	{
+		Stack_Push(Stack, '1');
+		Haffuman_encode(root->rChild, Stack);
+	}
+	Stack_Pop(Stack);
+}
+/*
+	哈夫曼解码：
+		将传进来的str('0','1')字符串，挨个遍历，查找对应的字符(按照左0，右1的方式遍历)，
+		每次循环用Temp_root指向根节点，保证可以按照编码遍历到对于的字符。
+		当左右孩子为NULL，说明已经找到了目标字符所在的叶子节点。
+*/
+
+Stack_list* Haffuman_decode(Stack_list* pCurrent, int str)
+{
+
+	if (pCurrent->lChild != NULL && pCurrent->rChild != NULL)
+	{
+		if (str == 48)
+		{
+			pCurrent = pCurrent->lChild;
+		}
+		else
+		{
+			pCurrent = pCurrent->rChild;
+		}
+	}
+	if (pCurrent->lChild == NULL && pCurrent->rChild == NULL)
+	{
+		Haffuman_ch = pCurrent->ch;
+	}
+	return pCurrent;
+}
+
+/*
+	创建有序链表(从小到大)：
+		当链表为空，插进来的节点即为头结点
+		当链表不为空，定义pCurrent，pPrevious两个指针，以便找到合适的插入位置
+		找到位置后修改指针即可
+*/
+Stack_list* Insert_list(Stack_list* root, Stack_list* insert_node)
+{
+	Stack_list* pCurrent;		//指向当前节点的指针
+	Stack_list* pPrevious;		//指向当前节点的前驱节点的指针
+	memset(insert_node->bit, '\0', sizeof(insert_node->bit));	//格式化bit数组空间
+	if (root == NULL)
+	{
+		root = insert_node;
+	}
+	else
+	{
+		pCurrent = root;		//当前节点指向头结点
+		pPrevious = NULL;
+		while (pCurrent && pCurrent->weight < insert_node->weight)	//找到对应的插入位置
+		{
+			pPrevious = pCurrent;
+			pCurrent = pCurrent->next;
+		}
+		insert_node->next = pCurrent;
+		if (pPrevious == NULL)		//前驱节点为NULL说明，其本身就是头结点
+		{
+			root = insert_node;
+		}
+		else
+		{
+			pPrevious->next = insert_node;
+		}
+	}
+
+	return root;
+}
+
+/*
+	创建哈夫曼树：
+		规定：较小的书放左节点，较大的数放右节点
+		根据创建哈夫曼的规则建立哈夫曼树
+*/
+Stack_list* Create_Huffman_Tree(Stack_list* root)
+{
+	Stack_list* L_Child;
+	Stack_list* R_Child;
+	//printf("Parent	lChild	rChild\n");
+	while (root && root->next)
+	{
+		Stack_list* newNode;
+		L_Child = root;
+		//printf("%d", L_Child->date);
+		R_Child = root->next;
+		//printf("%d", R_Child->date);
+		root = R_Child->next;
+
+		newNode = (Stack_list*)malloc(sizeof(Stack_list));
+		newNode->weight = L_Child->weight + R_Child->weight;
+		//printf("%4d,%6d,%6d\n",newNode->date,L_Child->date, R_Child->date);
+		newNode->lChild = L_Child;
+		newNode->rChild = R_Child;
+		newNode->next = NULL;
+		L_Child->next = NULL;
+		//L_Child->lChild = L_Child->rChild = NULL;
+		R_Child->next = NULL;
+		//R_Child->lChild = R_Child->rChild = NULL;
+		root = Insert_list(root, newNode);		//将新的节点插入有序链表
+	}
+	return root;
+}
+
+//中序遍历打印哈夫曼树
+void Print_HT_Tree(Stack_list* root)
+{
+	//printf("%d", root->date);
+	if (root == NULL)
+	{
+		return;
+	}
+
+	Print_HT_Tree(root->lChild);
+	if (root->lChild == NULL || root->rChild == NULL)
+	{
+		aver_code_length += root->weight * strlen(root->bit);
+		printf("  %c       %f   %s\n", root->ch, root->weight, root->bit);
+	}
+	Print_HT_Tree(root->rChild);
+
+}
+//打印哈夫曼树叶子节点的权值
+void Print(Stack_list* root)
+{
+	Stack_list* p = root;
+
+	if (!p)
+	{
+		return;
+	}
+	else {
+		while (p)
+		{
+			printf("%f", p->weight);
+			p = p->next;
+		}
+
+	}
+}
+void encode_tofile(Stack_list* root, char str)
+{
+
+	if (root == NULL)
+	{
+		return;
+	}
+	encode_tofile(root->lChild, str);
+	if (root->lChild == NULL && root->rChild == NULL)
+	{
+		if (str == root->ch)
+		{
+			strcpy_s(Haffuman_code, sizeof(Haffuman_code) / sizeof(Haffuman_code[0]), root->bit);
+		}
+	}
+	encode_tofile(root->rChild, str);
+}
 int main()
 {
-	printf("=================香农编码====================\n");
-	//初始化结构体数组
-	Shannon_node bit_code[LENGTH];
-	for (int i = 0; i < LENGTH; i++)
-	{
-		memset(bit_code[i].bit, '\0', sizeof(bit_code[i].bit));
-		bit_code[i].code_length = 0;
-		bit_code[i].probability = 0.0;
-		bit_code[i].sum_probability = 0.0;
-		bit_code[i].ch = 'A';
-	}
-	int code_length[27] = { 0 };	//存储码长
 	int num[27] = { 0 };	//存放字符出现的次数
 	char ahp[27] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ' };
 	errno_t err_read;
 	errno_t err_write;
 	errno_t err_write_read;
-	FILE *fp_read;			//读文件指针
-	FILE *fp_write;			//写文件指针
+	FILE *fp_read;
+	FILE *fp_write;
 	FILE *fp_write_read;
-	if ((err_read = fopen_s(&fp_read, "G:\\信息论\\text1.txt", "r+")) != 0)		//如果打开失败，打印失败信息
+	if ((err_read = fopen_s(&fp_read, "G:\\信息论\\text1.txt", "r+")) != 0)
 	{
 		printf("error\n");
 	}
@@ -75,15 +279,13 @@ int main()
 	{
 		printf("error\n");
 	}
-	char buf[1024] = { '\0' };		//定义数据缓冲区
-	char temp_buf[1024] = { '\0' };		//定义临时存储数据缓冲区
+	char buf[1024] = { '\0' };
+	char temp_buf[1024] = { '\0' };
 	int fp_index = 0;
-	while (fgets(buf, (1024), fp_read) != NULL)		//一次读取1024长度的数据
+	while (fgets(buf, (1024), fp_read) != NULL)
 	{
-		//对数据进行数据处理
 		for (int i = 0; i < sizeof(buf) / sizeof(buf[0]); i++)
 		{
-			//将大写改成小写
 			if (buf[i] >= 'A' && buf[i] <= 'Z')
 			{
 				temp_buf[fp_index++] = buf[i] + 32;
@@ -97,13 +299,11 @@ int main()
 				temp_buf[fp_index++] = buf[i];
 			}
 		}
-		//将temp_buf数据写入文件
 		fputs(temp_buf, fp_write);
 		fp_index = 0;
 	}
 	fclose(fp_read);
 	fclose(fp_write);
-	//预处理后进行概率统计
 	if ((err_write_read = fopen_s(&fp_write_read, "G:\\信息论\\text1_result.txt", "r+")) != 0)
 	{
 		printf("error\n");
@@ -124,49 +324,53 @@ int main()
 		}
 		memset(buf, '\0', sizeof(buf));
 	}
+	/*for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
+	{
+		printf("%d\n", num[i]);
+	}
+	printf("%d\n", sum);*/
 	fclose(fp_write_read);
 
 
 	//计算每个字符出现的概率
 	double probability_arr[27] = { 0.0 };
-	for (int i = 0; i < LENGTH; i++)
+	for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
 	{
 		probability_arr[i] = num[i] / (double)sum;
-		bit_code[i].probability = probability_arr[i];
-		bit_code[i].ch = ahp[i];
 	}
-	//计算码长
+	/*for (int i = 0; i < sizeof(probability_arr) / sizeof(probability_arr[0]); i++)
+	{
+		printf("%f\n", probability_arr[i]);
+	}*/
+	//计算信源熵
+	double source_entropy = 0;
+	for (int i = 0; i < sizeof(probability_arr) / sizeof(probability_arr[0]); i++)
+	{
+		source_entropy += (probability_arr[i] * (-log10(probability_arr[i]) / log10(2)));
+	}
+	Stack_Code* Stack = (Stack_Code*)malloc(sizeof(Stack_Code));
+	memset(Stack->code, '\0', sizeof(Stack->code));
+	init(Stack);
+	int result[16] = { 0 };
+	//int arr_weight[] = { 9,1,3,5,7,0 };
+	//char arr_ch[] = { 'A','B','C','D','E','F' };
+	Stack_list* root = NULL;
+	Stack_list* result_root = NULL;
 	for (int i = 0; i < sizeof(ahp) / sizeof(ahp[0]); i++)
 	{
-		code_length[i] = (int)((-log10(probability_arr[i]) / log10(2)) + 1);
-		bit_code[i].code_length = code_length[i];
+		Stack_list* insert_node = (Stack_list*)malloc(sizeof(Stack_list));
+		insert_node->ch = ahp[i];
+		insert_node->weight = probability_arr[i];
+		insert_node->lChild = NULL;
+		insert_node->next = NULL;
+		insert_node->rChild = NULL;
+		Stack_list* result_root = Insert_list(root, insert_node);
+		root = result_root;
 	}
-	sort(bit_code);
+	root = Create_Huffman_Tree(root);
+	Haffuman_encode(root, Stack);
 
-
-	//计算累加概率
-	//double sum_pro[27] = { 0.0 };
-	for (int i = 0; i < LENGTH - 1; i++)
-	{
-		Pro_sum += bit_code[i].probability;
-		bit_code[i + 1].sum_probability = Pro_sum;
-	}
-	double result_pro;
-	for (int i = 0; i < LENGTH; i++)
-	{
-		result_pro = bit_code[i].sum_probability;
-		for (int j = 0; j < bit_code[i].code_length; j++)
-		{
-
-			int temp = (int)(result_pro * 2);
-			bit_code[i].bit[j] = temp + 48;
-			result_pro = (result_pro * 2) - (int)(result_pro * 2);
-
-		}
-	}
-
-
-	//对文件进行编码
+	//读取文件进行编码
 	if ((err_read = fopen_s(&fp_read, "G:\\信息论\\text1_result.txt", "r+")) != 0)
 	{
 		printf("error\n");
@@ -175,23 +379,24 @@ int main()
 	{
 		printf("error\n");
 	}
-	memset(buf, '\0', sizeof(buf));
 	while (fgets(buf, 1024, fp_read) != NULL)
 	{
 		for (int i = 0; i < sizeof(buf) / sizeof(buf[0]); i++)
 		{
-			encode_tofile(bit_code, buf[i]);
-			fputs(Shannon_code, fp_write);
-			//fputc(flag, fp_write);
-			memset(Shannon_code, '\0', sizeof(Shannon_code));
-
+			encode_tofile(root, buf[i]);
+			fputs(Haffuman_code, fp_write);
+			memset(Haffuman_code, '\0', sizeof(Haffuman_code));
 		}
 		memset(buf, '\0', sizeof(buf));
 	}
 	fclose(fp_read);
 	fclose(fp_write);
 
-	//读取文件解码
+	//读取文件，解码
+	int ch;
+	Stack_list* temp_pCurrent;
+	Stack_list* temp_root;
+	temp_root = root;
 	if ((err_read = fopen_s(&fp_read, "G:\\信息论\\text1_encode.txt", "r+")) != 0)
 	{
 		printf("error\n");
@@ -200,75 +405,31 @@ int main()
 	{
 		printf("error\n");
 	}
-	int i = 0, j = 0, k = 0, flag_count = 0;
-	int count = 0;
-	char buffer[80000] = { '\0' };
-	while (fgets(buffer, 80000, fp_read) != NULL)
+	while ((ch = fgetc(fp_read)) != EOF)
 	{
-		i = 0;
-		while (buffer[i] != '\0')
+		temp_pCurrent = Haffuman_decode(temp_root, ch);
+		temp_root = temp_pCurrent;
+		if (temp_pCurrent->lChild == NULL && temp_pCurrent->rChild == NULL)
 		{
-
-			if (bit_code[k].bit[j] == '\0')
-			{
-				fputc(bit_code[k].ch, fp_write);
-				j = 0;
-				k++;
-				if (k > LENGTH - 1)
-				{
-					k = 0;
-				}
-			}
-			if (buffer[i] == bit_code[k].bit[j])
-			{
-				if (buffer[i + 1] == '\0')
-				{
-					j++;
-					break;
-				}
-
-				i++;
-				j++;
-			}
-			else
-			{
-
-
-				i = i - j;
-				k++;
-				if (k > LENGTH - 1)
-				{
-					k = 0;
-				}
-				j = 0;
-			}
+			fputc(Haffuman_ch, fp_write);
+			//memset(Haffuman_ch, '\0', sizeof(Haffuman_ch));
+			temp_root = root;
 		}
-		count++;
-		memset(buf, '\0', sizeof(buf));
+
 	}
 	fclose(fp_read);
 	fclose(fp_write);
-	//计算信源熵
-	double source_entropy = 0;
-	for (int i = 0; i < sizeof(probability_arr) / sizeof(probability_arr[0]); i++)
-	{
-		source_entropy += (probability_arr[i] * (-log10(probability_arr[i]) / log10(2)));
-	}
-	//平均码长
-	for (int i = 0; i < sizeof(probability_arr) / sizeof(probability_arr[0]); i++)
-	{
-		aver_code_length += (code_length[i] * probability_arr[i]);
-	}
-	//编码效率
+	printf("信源字符   权值       码字\n");
+	Print_HT_Tree(root);
+	printf("哈夫曼编码信源熵：%f\n", source_entropy);
+	//计算编码效率
 	double efficiency = 0; //初始化编码效率
 	efficiency = source_entropy / aver_code_length;
-	//打印信息
-	for (int i = 0; i < LENGTH; i++)
-	{
-		printf("%c,%f,%d,%f,%s\n", bit_code[i].ch, bit_code[i].probability, bit_code[i].code_length, bit_code[i].sum_probability, bit_code[i].bit);
-	}
-	printf("香农编码信源熵  ：%f\n", source_entropy);
-	printf("香农编码编码效率：%0.3f%%\n", efficiency * 100);
-	//printf("%s", Shannon_code);
+	printf("哈夫曼编码效率  ：%0.3f%%\n", efficiency * 100);
+	//printf("%f\n", aver_code_length);
+	//Haffuman_decode(root, Haffuman_code);
+	//printf("\n%s\n", Haffuman_code);
 	system("pause");
+
+
 }
